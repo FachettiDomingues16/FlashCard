c57ef9bae729608ea49d89603013f656248aa6fa
FachettiDomingues16 <fachetti.domingues@escola.pr.gov.br>
2025-08-14 15:32:14 -0300

    Add files via upload

---
+++ Perguntas.js
@@ -0,0 +1,46 @@
+criaCartao(
+    'Educação Física',
+    'Onde foi criado o Taekwondo?',
+    'Na Coreia do Sul'
+)
+criacartao(
+    'Educação Física',
+    'Quais são as 8 armas do Muay Thai?',
+    'As 8 armas do Muay Thai são dois punhos, dois cotovelos, dois joelhos e duas canelas/pés.'
+)
+criacartao(
+    'Educação Física',
+    'Quem é considerado o fundador do Brazilian Jiu-Jitsu (BJJ)?',
+    'Carlos Gracie.'
+)
+criacartao(
+    'Historia',
+    'Qual foi a causa da Revolução Francesa?',
+    'Desigualdade social e crise financeira do governo.'
+
+)
+criacartao(
+    'Educação Física',
+    'Quais são os principais benefícios da prática regular de atividades físicas?',
+    'Melhora a qualidade de vida, previne doenças crônicas, controla o peso e reduz estresse, ansiedade e depressão.'
+)
+criacartao(
+    'Robótica',
+    'O que é um sensor em robótica?',
+    'Um sensor em robótica é um dispositivo que detecta e mede o ambiente.'
+)
+criacartao(
+    'Futebol',
+    'Quem foi o maior artilheiro da história do Palmeiras?',
+    'O maior artilheiro da história do Palmeiras é Alberto Valentim.'
+)
+criacartao(
+    'Educação Física',
+    'Qual o melhor exercício para desenvolver a parte superior do peitoral na academia?',
+    'Supino inclinado.'
+)
+criacartao(
+    'Educação Física',
+    'Qual o melhor exercício para desenvolver o bíceps?',
+    'Curl de bíceps/Rosca direta'
+)
