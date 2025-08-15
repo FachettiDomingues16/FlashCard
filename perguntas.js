7dde218d07fac178cac472748d9c62f328266c66
FachettiDomingues16 <fachetti.domingues@escola.pr.gov.br>
2025-08-15 14:35:04 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,45 +1,52 @@
 criaCartao(
     'Educação Física',
     'Onde foi criado o Taekwondo?',
-    'Na Coreia do Sul'
+    'Na Coreia do Sul.'
 )
-criacartao(
+
+criaCartao(
     'Educação Física',
     'Quais são as 8 armas do Muay Thai?',
     'As 8 armas do Muay Thai são dois punhos, dois cotovelos, dois joelhos e duas canelas/pés.'
 )
-criacartao(
+
+criaCartao(
     'Educação Física',
     'Quem é considerado o fundador do Brazilian Jiu-Jitsu (BJJ)?',
     'Carlos Gracie.'
 )
-criacartao(
+
+criaCartao(
     'Historia',
     'Qual foi a causa da Revolução Francesa?',
     'Desigualdade social e crise financeira do governo.'
 
 )
-criacartao(
+criaCartao(
     'Educação Física',
     'Quais são os principais benefícios da prática regular de atividades físicas?',
     'Melhora a qualidade de vida, previne doenças crônicas, controla o peso e reduz estresse, ansiedade e depressão.'
 )
-criacartao(
+
+criaCartao(
     'Robótica',
     'O que é um sensor em robótica?',
     'Um sensor em robótica é um dispositivo que detecta e mede o ambiente.'
 )
-criacartao(
+
+criaCartao(
     'Futebol',
     'Quem foi o maior artilheiro da história do Palmeiras?',
     'O maior artilheiro da história do Palmeiras é Alberto Valentim.'
 )
-criacartao(
+
+criaCartao(
     'Educação Física',
     'Qual o melhor exercício para desenvolver a parte superior do peitoral na academia?',
     'Supino inclinado.'
 )
-criacartao(
+
+criaCartao(
     'Educação Física',
     'Qual o melhor exercício para desenvolver o bíceps?',
     'Curl de bíceps/Rosca direta'
